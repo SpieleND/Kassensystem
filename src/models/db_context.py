b690142f54435adf94565a62631ad12f6b3d4524
Matthias Gabel <matthias-gabel@hotmail.de>
2025-03-24 21:44:29 +0100

    update context and add docker-compose file

--- src/models/db_context.py
+++ src/models/db_context.py
@@ -7,10 +7,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = (
-    f"mysql+pymysql://{os.getenv('DB_USER')}:"
-    f"{os.getenv('DB_PASSWORD')}@"
-    f"{os.getenv('DB_HOST')}/"
-    f"{os.getenv('DB_NAME')}"
+    f"mysql+pymysql://{os.getenv('MYSQL_USER')}:"
+    f"{os.getenv('MYSQL_PASSWORD')}@"
+    f"{os.getenv('MYSQL_HOST')}/"
+    f"{os.getenv('MYSQL_DATABASE')}"
 )
 
 engine = create_engine(DATABASE_URL)
